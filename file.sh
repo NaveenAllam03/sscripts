73459506a8e27c520535457ea39bdb923e34c0b4
Naveen <naveenallam99@gmail.com>
2024-08-22 10:17:39 +0530

    checking file with script

---
+++ file.sh
@@ -0,0 +1,14 @@
+#1/bin/bash
+
+#####################
+# checking weather a file is present or not through shellscript with hardcode value..
+# 
+# enter a file name  
+# write a if statement to validate..
+#
+#####################
+
+if [ -f /etc/group ]
+then 
+    echo " file is avaliable "
+fi
