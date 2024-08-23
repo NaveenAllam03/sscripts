d8a325eaa2126ede15886cfd8ff12624a323316e
Naveen <naveenallam99@gmail.com>
2024-08-23 14:11:27 +0530

    sample script

--- for1.sh
+++ for1.sh
@@ -8,6 +8,16 @@
 
 for a in {1..5}
 do
-echo $(time)
+echo $(date)
+sleep 3
 done
 
+##########
+# to print only specific things
+# 
+# for a in {1..10}
+# do
+# echo $(date) | awk -F " " '{print $1, $1, $3, $4}'
+# done
+#
+##########
