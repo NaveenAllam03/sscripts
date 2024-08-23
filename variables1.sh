14478a2635f811db48b6ce75a6b0e931b10716e2
Naveen <naveenallam99@gmail.com>
2024-08-23 10:42:08 +0530

    taking commands input variable in a shell script

---
+++ variables1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##########################
+# Running a command in shell script and taking that as an input
+# 
+#
+#
+#
+#
+#
+##########################
+
+month=$(cal)
+echo "month is : ${$month} "
