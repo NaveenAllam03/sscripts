9b2ffd1fca7f12a79e56b54786beb780fee2db5b
Naveen <naveenallam99@gmail.com>
2024-08-24 11:24:28 +0530

    worked sucessfully

--- 01nginx.sh
+++ 01nginx.sh
@@ -3,17 +3,11 @@
 ########################
 # wrote a function and called it
 # checked and validated before the software installation
-# 
-#
-#
-#
-#
-#
 #########################
 
 user=$(id -u)
 
-CHECKING(){
+CHECKING(){              # function for repetetive code and calling it to validate exit status of commands
     if [ $1 -ne 0 ]
     then 
        echo " $2 Failed to install..."
@@ -23,7 +17,7 @@ CHECKING(){
     fi             
 }
 
-if [ $user -ne 0 ]
+if [ $user -ne 0 ]       #condition checking for sudo access
 then
     echo "change to sudo user to run..."
     exit 1 
@@ -31,15 +25,15 @@ else
     echo "executing command with sudo access..."  
 fi
 
-apt-get update -y
+apt-get update -y        #updating packages 
 
-apt-get install nginx -y
+apt-get install nginx -y # installing nginx 
 
-CHECKING $? "Nginx"
+CHECKING $? "Nginx"      #validating installed or not with passing arguments to function.
 
-apt-get install git -y
+apt-get install git - y  #installing git
 
-CHECKING $? "git"
+CHECKING $? "git"        # validating installed or not with passing arguments to funtion.
 
 
 
