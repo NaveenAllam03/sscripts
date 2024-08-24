7605b2cac57958de26e6776b3fe8976d2e689c1e
Naveen <naveenallam99@gmail.com>
2024-08-24 13:37:52 +0530

    changed naming convention

--- 01specialvariable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "print all arguments : $@"
-echo "arguments passed in numbers : $#"
-echo "show scriptname : $0"
---
+++ specialvariable01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+################
+# echo "print all arguments : $@"
+# echo "arguments passed in numbers : $#"
+# echo "show scriptname : $0"
+#################
+
