07dea4626f678719a9916754e041e3f8433747fe
Naveen <naveenallam99@gmail.com>
2024-08-25 10:16:30 +0530

    test

---
+++ installpackages01.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+######################
+# installing packages using for loop and special variables 
+# and validating those packages using functions
+# 
+#
+#
+#
+#
+######################
+
+USER=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "started installing..."
+    else
+        echo "already exists..."
+    fi    
+}
+
+
+if [ $USER -ne 0 ]
+then
+    echo "run script as sudo user..."
+    exit 1
+else
+    echo "executing scripts as sudo user..."
+fi   
+
+apt-get update 
